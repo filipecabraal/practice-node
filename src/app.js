58aba40fbe0a230af3d2df38a6cd868e6d0178b7
Filipe Cabral <filipecabraal@gmail.com>
2019-03-29 14:41:14 -0300

    Organize controllers and routes

--- src/app.js
+++ src/app.js
@@ -5,37 +5,14 @@ const bodyParser = require('body-parser'); //Module to convert the body in json.
 const app = express(); //Add the "express" module in a variable (to start it).
 const router = express.Router(); //Starting the route that can be used.
 
+// Loading routers.
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
 app.use(bodyParser.json()); // All content of the body converted in json.
 app.use(bodyParser.urlencoded({extended: false})); // Adjust URL.
 
-
-// Create the route with the method to send.
-const route = router.get( '/', (req, res, next) => {
-	res.status(200).send({
-		title: 'Node Store API',
-		version: '0.0.1'
-	});
-});
-
-const create = router.post( '/', (req, res, next) => {
-	res.status(201).send(req.body);
-});
-
-const put = router.put( '/:id', (req, res, next) => {
-	let id = req.params.id;
-	res.status(200).send({
-		id: id,
-		item: req.body
-	});
-});
-
-const del = router.delete( '/', (req, res, next) => {
-	res.status(200).send(req.body);
-});
-
-app.use('/', route); // Set the route to get on app.
-app.use('/products', create); // Set the route to post on app.
-app.use('/products', put); // Set the route to put on app.
-app.use('/products', del); // Set the route to delete on app.
+app.use('/', indexRoute); // Set the route to get on app.
+app.use('/products', productRoute); // Set the route to post on app.
 
 module.exports = app; // Export the application.
